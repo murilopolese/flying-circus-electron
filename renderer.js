f30a23104a10bf72c1cc9e7d6aaf3fb879a2403d
Murilo Polese <murilopolese@gmail.com>
2018-07-12 19:53:40 +0100

    Switching to connection classes
    And implementing both serial and websocket connections

--- renderer.js
+++ renderer.js
@@ -1,95 +1,22 @@
 'use strict'
 const fs = require('fs')
-
-const SerialPort = require('serialport')
-const EventEmitter = require('events')
-const Board = require('./mpboard.js')
-const bus = new EventEmitter()
-
-let board
-
-bus.on('connect', (options) => {
-    if (!options) {
-        return
-    }
-    board = new Board(options)
-    // Events received by the board and proxied to front end
-    board.on('connected', () => {
-        bus.emit('connected')
-        board.sendStop()
-    })
-    board.on('disconnected', () => {
-        bus.emit('disconnected')
-    })
-    board.on('raw-repl-started', () => {
-        bus.emit('raw-repl-started')
-    })
-    board.on('raw-repl-finished', () => {
-        bus.emit('raw-repl-finished')
-    })
-    board.on('raw-exec-done', () => {
-        bus.emit('raw-exec-done')
-    })
-    board.on('output', (data) => {
-        bus.emit('output', data)
-    })
-})
-bus.on('disconnect', (options) => {
-    try {
-        board.close()
-    } catch (e) {
-        console.log('error', e);
-    }
-    board = null
-    bus.emit('device-disconnected')
-    bus.emit('output', 'Disconected\n\r')
-})
-
-// Events received from the front end to call board functions
-bus.on('enter-raw-repl', () => {
-    board.enterRawRepl()
-})
-bus.on('exit-raw-repl', () => {
-    board.exitRawRepl()
-})
-bus.on('exec-raw', (code) => {
-    board.execRaw(code)
-})
-bus.on('send-stop', () => {
-    board.sendStop()
-})
-bus.on('send-reset', () => {
-    board.softReset()
-})
-bus.on('exec-from-string', (code) => {
-    board.execFromString(code)
-})
-bus.on('exec', (command) => {
-    board.exec(command)
-})
-
-bus.on('load-available-devices', () => {
-    SerialPort.list().then((ports) => {
-        bus.emit('available-devices', ports.filter((port) => {
-            return !!port.vendorId
-        }))
-    })
-})
-
-bus.on('save-file', (data) => {
-    console.log('save file', data)
-    board.saveFile(data.filename, data.code)
-})
-bus.on('load-file', (filename) => {
-    board.loadFile(filename)
-})
-bus.on('remove-file', (filename) => {
-    board.removeFile(filename)
-})
-
-window.FlyingCircus = { bus }
+const WebsocketConnection = require('flying-circus-connection-websocket')
+const SerialConnection = require('flying-circus-connection-serial')
 
 window.onload = () => {
+    // CREATE FLYING CIRCUS COMPONENT
+    let flyingCircus = document.createElement('flying-circus-ide')
+
+    // CONFIGURING CONNECTIONS
+    WebsocketConnection.prototype._saveAs = (path, data) => {
+        flyingCircus.set('code', new TextDecoder('utf-8').decode(data))
+    }
+
+    flyingCircus.WebsocketConnection = WebsocketConnection
+    flyingCircus.SerialConnection = SerialConnection
+
+
+    // LOAD FILES BY DRAGGING IT INTO ELECTRON WINDOW
     const dragOverHandler = (e) => {
         e.preventDefault()
         return false
@@ -107,12 +34,16 @@ window.onload = () => {
         const path = e.dataTransfer.files[0].path
         if (path) {
             const code = fs.readFileSync(path)
-            bus.emit('load-code', code.toString())
+            flyingCircus.set('code', code.toString())
         }
         return false
     }
-
     window.ondragover = dragOverHandler
     window.ondragleave = dragLeaveHandler
     window.ondrop = dropHandler
+
+
+    // APPENDING FLYING CIRCUS UI TO DOCUMENT
+    document.body.appendChild(flyingCircus)
+
 }
